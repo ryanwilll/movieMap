6fb075a811052a4da66ffb3e6246919d21431a72
Ryan Will Darós <ryan_will-@hotmail.com>
2023-09-18 22:38:04 -0300

    [FIX] Criado context para manter dados carregados

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { MoviesProvider } from './context/MoviesContext'
+
 import './App.css'
 import Home from './pages/Home/Home'
 import Navbar from './components/Navbar/Navbar'
@@ -8,16 +10,18 @@ import Movies from './pages/Movies/Movies'
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="series" element={<Home />} />
-          <Route path="search" element={<Home />} />
-          <Route path=":type/details/:id" element={<Details />} />
-        </Routes>
-      </BrowserRouter>
+      <MoviesProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="movies" element={<Movies />} />
+            <Route path="series" element={<Home />} />
+            <Route path="search" element={<Home />} />
+            <Route path=":type/details/:id" element={<Details />} />
+          </Routes>
+        </BrowserRouter>
+      </MoviesProvider>
     </>
   )
 }
