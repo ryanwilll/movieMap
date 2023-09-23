f634ae7ee2cc5344bfc6cb79404edcad763c1d69
Ryan Will Darós <ryan_will-@hotmail.com>
2023-09-23 00:04:04 -0300

    [REFACTORING]Refatoração em todos os componentes

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -13,20 +13,10 @@ type Props = {
 }
 
 export const Modal = ({ isOpen, item_id, type, setIsOpen }: Props) => {
-  const [dublados, setDublados] = useState<any>()
-
   //! Chamar a API aqui
-  const { data: dataPtBR } = useFetch(
-    `/${type == 'serie' ? 'tv' : 'movie'}/${item_id}/videos?language=pt-BR&append_to_response=videos`
-  )
-  const { data: dataEnUS } = useFetch(
-    `/${type == 'serie' ? 'tv' : 'movie'}/${item_id}/videos?language=pt-BR&append_to_response=videos`
-  )
-
-  useEffect(() => {
-    const trailersDublados = dataPtBR?.results.filter((item) => item.name.toLowerCase().includes('dublado'))
-    setDublados(trailersDublados)
-  }, [dataPtBR])
+  const { data: dataPtBR } = useFetch(`/${type == 'serie' ? 'tv' : 'movie'}/${item_id}/videos?language=pt-BR`)
+  const { data: dataEnUS } = useFetch(`/${type == 'serie' ? 'tv' : 'movie'}/${item_id}/videos?language=en-US`)
+  const trailersDublados = dataPtBR?.results.filter((item) => item.name.toLowerCase().includes('dublado'))
 
   useEffect(() => {
     const body = document.querySelector('body')
@@ -44,21 +34,20 @@ export const Modal = ({ isOpen, item_id, type, setIsOpen }: Props) => {
 
   const content = (
     <div className={styles.modal} onClick={closeModal}>
-      {dublados && dataEnUS && (
+      {trailersDublados && dataEnUS && (
         <motion.div
           initial={{ y: 100, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           onClick={(e) => e.stopPropagation()}
           className={styles.modal_container}>
-          {dublados?.[0] == undefined && dataEnUS!.results?.length <= 0 ? (
+          {trailersDublados?.[0] == undefined && dataEnUS!.results?.length <= 0 ? (
             <h1 className={styles.modal_notfound}>Não foi possível encontrar um trailer.</h1>
           ) : (
             <>
-              <h2>Trailer em: {dublados?.[0] ? 'Português' : 'Inglês'}</h2>
               <iframe
                 className={styles.iframe}
                 src={`https://www.youtube.com/embed/${
-                  dublados?.[0]?.key ? dublados?.[0]?.key : dataEnUS?.results?.[0]?.key
+                  trailersDublados?.[0]?.key ? trailersDublados?.[0]?.key : dataEnUS?.results?.[0]?.key
                 }?si=Bz5t4-fuIGcRbOJI`}
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,11 +1,11 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
 import { LuCalendarDays } from 'react-icons/lu'
 import { MdHighQuality } from 'react-icons/md'
 import { AiFillStar } from 'react-icons/ai'
+import styles from './MovieCard.module.css'
 
 import noPosts from '../../assets/no-poster.png'
-
-import styles from './MovieCard.module.css'
 
 type Props = {
   id: number
@@ -22,21 +22,24 @@ const URL_IMAGE = import.meta.env.VITE_IMG
 
 const MovieCard = ({ id, title, poster, date, averange, type }: Props) => {
   const navigate = useNavigate()
+
   const formatedDate = (data: string) => {
     const date = new Date(data)
-
     const formattedDate = date.toLocaleDateString('pt-BR', {
       day: '2-digit',
       month: '2-digit',
       year: 'numeric',
     })
-
     return formattedDate
+  }
+
+  const goToDetailsPage = () => {
+    navigate(type == 'movie' ? `/movie/details/${id}` : `/serie/details/${id}`)
   }
 
   return (
     <>
-      <Link to={type == 'movie' ? `movie/details/${id}` : `serie/details/${id}`} className={styles.container}>
+      <div onClick={() => goToDetailsPage()} className={styles.container}>
         <img src={poster ? `${URL_IMAGE}${poster}` : noPosts} alt={`Imagem ilustrativa do filme ${title}`} />
         <div className={styles.wrapper}>
           <p className={styles.title}>{title}</p>
@@ -55,7 +58,7 @@ const MovieCard = ({ id, title, poster, date, averange, type }: Props) => {
             <span>{averange.toFixed(1)}</span>
           </p>
         </div>
-      </Link>
+      </div>
     </>
   )
 }
