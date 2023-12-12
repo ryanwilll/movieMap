ddf9891dcf7ec72e058b1b7e9c2de9f1d09a4ecc
Ryan Will <ryan_will-@hotmail.com>
2023-12-11 23:04:53 -0300

    iniciado reformulação/adicionado navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,81 +1,108 @@
-import { BiMenu, BiX } from 'react-icons/bi'
+import { BiMenu, BiSearch, BiX } from 'react-icons/bi';
 
-import '../../sass/index.scss'
-import { useState, useEffect, useRef } from 'react'
-import { Link } from 'react-router-dom'
+import '../../sass/index.scss';
+import { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  const [isMobileView, setIsMobileView] = useState<boolean | null>(null)
-  const [isOpenMenuMobile, setIsOpenMenuMobile] = useState<boolean | null>(null)
+  const [isMobileView, setIsMobileView] = useState<boolean | null>(null);
+  const [isOpenMenuMobile, setIsOpenMenuMobile] = useState<boolean | null>(null);
+  const [isOpenSearch, setIsOpenSearch] = useState<boolean | null>(null);
 
-  const menuRef = useRef<HTMLDivElement>(null)
+  const menuRef = useRef<HTMLDivElement>(null);
+  const searchRef = useRef<HTMLDivElement>(null);
 
   const handleOpenOrCloseMenu = () => {
-    setIsOpenMenuMobile((prev) => !prev)
-    menuRef?.current?.classList.toggle('hidden')
-  }
+    setIsOpenMenuMobile((prev) => !prev);
+    console.log('clicou');
+    menuRef?.current?.classList.toggle('hidden');
+  };
+
+  const handleCloseMenu = (ref: HTMLDivElement) => {
+    ref?.classList.add('hidden');
+  };
+
+  const handleOpenSearch = () => {
+    setIsOpenSearch((prev) => !prev);
+    console.log('clicou');
+    searchRef?.current?.classList.toggle('hidden');
+    console.log(searchRef?.current);
+  };
 
   useEffect(() => {
     const getWindowSize = () => {
-      const windowWidth = window.innerWidth
+      const windowWidth = window.innerWidth;
 
       if (windowWidth < 1024) {
-        setIsMobileView(true)
+        setIsMobileView(true);
       } else {
-        setIsMobileView(false)
-        setIsOpenMenuMobile(false)
+        setIsMobileView(false);
       }
-    }
-    getWindowSize()
+    };
+    getWindowSize();
 
-    window.addEventListener('resize', getWindowSize)
-  }, [])
+    window.addEventListener('resize', getWindowSize);
+  }, []);
 
   return (
     <nav>
-      <div className="container__layout">
-        <Link to="/" className="brand">
-          <img className="navbar__image" src="/Logo.svg" alt="Logotipo do MovieMap" />
+      <div className='container__layout'>
+        <Link to='/' className='brand'>
+          <img className='navbar__image' src='/Logo.svg' alt='Logotipo do MovieMap' />
           <h1>MovieMap</h1>
         </Link>
 
         <div>
-          {isMobileView && !isOpenMenuMobile && <BiMenu onClick={handleOpenOrCloseMenu} />}
-          {isMobileView && isOpenMenuMobile && <BiX onClick={handleOpenOrCloseMenu} />}
+          <>
+            <BiSearch onClick={handleOpenSearch} />
+            <BiMenu onClick={handleOpenOrCloseMenu} />
+          </>
+
+          {/* {isMobileView && !isOpenMenuMobile && !isOpenSearch && <BiX onClick={() => handleCloseMenu(menuRef?.current)} />} */}
 
           {!isMobileView && (
-            <div className="container__layout__navbar__links">
-              <Link className="navbar__link" to={'/'}>
+            <div className='container__layout__navbar__links'>
+              <Link className='navbar__link' to={'/'}>
                 Home
               </Link>
-              <Link className="navbar__link" to={'/1'}>
+              <Link className='navbar__link' to={'/1'}>
                 Home 1
               </Link>
-              <Link className="navbar__link" to={'/2'}>
+              <Link className='navbar__link' to={'/2'}>
                 Home 2
               </Link>
-              <Link className="navbar__link" to={'/3'}>
+              <Link className='navbar__link' to={'/3'}>
                 Home 3
               </Link>
-              <Link className="navbar__link" to={'/4'}>
+              <Link className='navbar__link' to={'/4'}>
                 Home 4
               </Link>
             </div>
           )}
         </div>
       </div>
-
       {isMobileView && (
-        <div className="menuMobile hidden" ref={menuRef}>
-          <a href="#">Item #1</a>
-          <a href="#">Item #2</a>
-          <a href="#">Item #3</a>
-          <a href="#">Item #4</a>
-          <a href="#">Item #5</a>
+        <div className='menuMobile hidden' ref={menuRef}>
+          <a href='#'>Item #1</a>
+          <a href='#'>Item #2</a>
+          <a href='#'>Item #3</a>
+          <a href='#'>Item #4</a>
+          <a href='#'>Item #5</a>
         </div>
       )}
+      {isMobileView && (
+        <>
+          <div className='menuMobileSearch hidden' ref={searchRef}>
+            <a href='#'>MenuMobileSearch #1</a>
+            <a href='#'>MenuMobileSearch #2</a>
+            <a href='#'>MenuMobileSearch #3</a>
+            <a href='#'>MenuMobileSearch #4</a>
+            <a href='#'>MenuMobileSearch #5</a>
+          </div>
+        </>
+      )}
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,54 +1,49 @@
-import { useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { fetchData } from '../../services/api'
-import { IMovies, ISeries } from '../../types/IMoviesOrSeries'
-import Card from '../../components/Card/Card'
+import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchData } from '../../services/api';
+import { IMovies, ISeries } from '../../types/IMoviesOrSeries';
+import Card from '../../components/Card/Card';
 
-import '../../sass/pages/_home.scss'
+import '../../sass/pages/_home.scss';
 
 const Home = () => {
-  const [type, setType] = useState<string>(localStorage.getItem('selectedType') || 'movie')
+  const [type, setType] = useState<string>(localStorage.getItem('selectedType') || 'movie');
 
   const { data: movies } = useQuery<IMovies[]>({
     queryKey: ['movies'],
     queryFn: () => fetchData('/movie/upcoming?language=pt-BR&page=1'),
-  })
+  });
 
   const { data: series } = useQuery<ISeries[]>({
     queryKey: ['tv'],
     queryFn: () => fetchData('/tv/top_rated?language=pt-BR&page=1'),
     enabled: type === 'tv',
-  })
+  });
 
   useEffect(() => {
-    localStorage.setItem('selectedType', type)
-  }, [type])
+    localStorage.setItem('selectedType', type);
+  }, [type]);
 
   return (
-    <div className="container">
+    <div className='container'>
       <>
         <div
           style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${movies?.[2].backdrop_path})` }}
-          className="home__container">
-          <div className="home__text">
-            <h1 className="brand">MovieMap</h1>
-            <p className="brand__description">Um guia completo com informações de todos os filmes e series.</p>
-            <form className="form__container">
-              <label htmlFor="search">O que você quer assistir?</label>
-              <input type="text" name="search" id="search" placeholder="Digite o nome do filme/série" />
-              <button className="btn__search">Search</button>
-            </form>
+          className='home__container'>
+          <div className='home__text'>
+            <h1 className='brand'>MovieMap</h1>
+            <p className='brand__description'>Um guia completo com informações de todos os filmes e series.</p>
           </div>
         </div>
-        <section className="section">
-          <div className="section__container">
+        <section className='section'>
+          <div className='section__container'>
             <div>
-              <h3 className="text__streamings">Disponíveis nos streamings online</h3>
-              <h2 className="text__descriptions">
+              <h3 className='text__streamings'>Disponíveis nos streamings online</h3>
+              <h2 className='text__descriptions'>
                 {type === 'movie' ? 'Filmes lançados recentemente' : 'Séries com as melhores avaliações'}
               </h2>
             </div>
-            <div className="btns__container">
+            <div className='btns__container'>
               <button className={`btn__option ${type === 'movie' && 'selected'}`} onClick={() => setType('movie')}>
                 Filmes
               </button>
@@ -57,7 +52,7 @@ const Home = () => {
               </button>
             </div>
           </div>
-          <div className="section__itens__grid">
+          <div className='section__itens__grid'>
             {type === 'movie' &&
               movies?.map((data) => (
                 <Card
@@ -88,7 +83,7 @@ const Home = () => {
         </section>
       </>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
